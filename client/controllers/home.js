d2c6c689d8e74c0d4dab9307d0f36a37ba7729b6
conor <conorbudge555@gmail.com>
2016-10-07 14:54:19 -0500

    Added Satellizer login

---
+++ client/controllers/home.js
@@ -0,0 +1,16 @@
+/**
+ * Created by conor on 10/5/16.
+ */
+
+angular.module('Instagram')
+    .controller('HomeCtrl', function($scope) {
+
+        $scope.isAuthenticated = function() {
+            // check if logged in
+        };
+
+        $scope.linkInstagram = function() {
+            // connect email account with instagram
+        };
+
+    });
